3e1873bab0aa0160a20b3790c428dfb5e0769a8f
wdhays2 <wdhays2@charter.net>
2017-11-08 13:28:45 -0800

    Update routes, add database design with definitions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'manufacturers#index'
   get 'manufacturers/index'
-
   get 'drivers/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
